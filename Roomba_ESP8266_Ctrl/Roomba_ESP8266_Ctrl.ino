18f8751b564c75d6e30a5d172511eb564d9fbdd3
Simone Luconi <luconi.simone@gmail.com>
2018-04-01 19:20:54 +0200

    Update Roomba_ESP8266_Ctrl.ino

--- Roomba_ESP8266_Ctrl/Roomba_ESP8266_Ctrl.ino
+++ Roomba_ESP8266_Ctrl/Roomba_ESP8266_Ctrl.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "Gandalf";
-const char* password = "simonecleomartina";
+const char* ssid = "WifiName";
+const char* password = "WifiPw";
 
 ESP8266WebServer server(80);
 
